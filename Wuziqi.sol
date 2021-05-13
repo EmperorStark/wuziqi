505e88440c9e17ae56ffa945586ce8aee280c874
Junhao Chen <junhaochenarthur@outlook.com>
2021-05-12 23:33:33 -0400

    Added Win Game Check and Restart

--- Wuziqi.sol
+++ Wuziqi.sol
@@ -4,42 +4,177 @@ contract Wuziqi{
     uint8[10][10] public board;
     address player1;
     address player2;
-    uint8 turn;
+    uint8 public turn;
 
     function fundContract() public payable{
     }
     
     function initiate(address opponent) public{
+        require(turn == 0);
         player1 = msg.sender;
         player2 = opponent;
         turn = 1;
     }
     
+    function restart() public{
+        require((msg.sender == player1 && turn == 1) || (msg.sender == player2 && turn == 2));
+        uint8[10][10] memory myArr;
+        board = myArr;
+        turn = 0;
+    }
+    
     function makeMove(uint8 x, uint8 y) public returns (uint8) {
+        require(x>=0 && x<10 && y>=0 && y<10);
         require(board[x][y] == 0, "already a piece here");
         require((msg.sender == player1 && turn == 1) || (msg.sender == player2 && turn == 2));
         board[x][y] = turn;
-        if (checkWin(x, y) && msg.sender == player1) {
-            return 1;
-        }else if (checkWin(x, y) && msg.sender == player2){
-            return 2;
-        }
+        uint8 winner = checkWin(x,y);
         if( turn == 1 ){
             turn = 2;
         }else {
             turn = 1;
         }
+        return winner;
     }
     
-    function checkWin(uint8 x, uint8 y) private returns (bool){
-        if(x == y){
-            return true;
-        }else{
-            return false;
+    function checkWin(uint8 x, uint8 y) view private returns (uint8){
+        uint8 degree0 = 0;
+        uint8 degree45 = 0;
+        uint8 degree90 = 0;
+        uint8 degree135 = 0;
+        if(x+1 < 10 && y+1 <10 && board[x+1][y+1] == turn){
+            degree135 += 1;
+            if(x+2 < 10 && y+2 <10 && board[x+2][y+2] == turn){
+            	degree135 += 1;
+            	if(x+3 < 10 && y+3 <10 && board[x+3][y+3] == turn){
+                	degree135 += 1;
+                	if(x+4 < 10 && y+4 <10 && board[x+4][y+4] == turn){
+                    	degree135 += 1;
+                    }
+                }
+            }
         }
+        if(degree135 >= 4){
+            return turn;
+        }
+        
+        if(x+1 < 10 && board[x+1][y] == turn){
+            degree90 += 1;
+            if(x+2 < 10 && board[x+2][y] == turn){
+            	degree90 += 1;
+            	if(x+3 < 10 && board[x+3][y] == turn){
+                	degree90 += 1;
+                	if(x+4 < 10 && board[x+4][y] == turn){
+                    	degree90 += 1;
+                    }
+                }
+            }
+        }
+        if(degree90 >= 4){
+            return turn;
+        }
+        
+        if(x+1 < 10 && y >= 1 && board[x+1][y-1] == turn){
+            degree45 += 1;
+            if(x+2 < 10 && y >= 2 && board[x+2][y-2] == turn){
+            	degree45 += 1;
+            	if(x+3 < 10 && y >= 3 && board[x+3][y-3] == turn){
+                	degree45 += 1;
+                	if(x+4 < 10 && y >= 4 && board[x+4][y-4] == turn){
+                    	degree45 += 1;
+                    }
+                }
+            }
+        }
+        if(degree45 >= 4){
+            return turn;
+        }
+        
+        if(y+1 < 10 && board[x][y+1] == turn){
+            degree0 += 1;
+            if(y+2 < 10 && board[x][y+2] == turn){
+            	degree0 += 1;
+            	if(y+3 < 10 && board[x][y+3] == turn){
+                	degree0 += 1;
+                	if(y+4 < 10 && board[x][y+4] == turn){
+                    	degree0 += 1;
+                    }
+                }
+            }
+        }
+        if(degree0 >= 4){
+            return turn;
+        }
+        
+        
+        if(x >= 1 && y >= 1 && board[x-1][y-1] == turn){
+            degree135 += 1;
+            if(x >= 2 && y >= 2 && board[x-2][y-2] == turn){
+            	degree135 += 1;
+            	if(x >= 3 && y >= 3 && board[x-3][y-3] == turn){
+                	degree135 += 1;
+                	if(x >= 4 && y >= 4 && board[x-4][y-4] == turn){
+                    	degree135 += 1;
+                    }
+                }
+            }
+        }
+        if(degree135 >= 4){
+            return turn;
+        }
+        
+        if(x >= 1 && board[x-1][y] == turn){
+            degree90 += 1;
+            if(x >= 2 && board[x-2][y] == turn){
+            	degree90 += 1;
+            	if(x >= 3 && board[x-3][y] == turn){
+                	degree90 += 1;
+                	if(x >= 4 && board[x-4][y] == turn){
+                    	degree90 += 1;
+                    }
+                }
+            }
+        }
+        if(degree90 >= 4){
+            return turn;
+        }
+        
+        if(x >= 1 && y+1 < 10 && board[x-1][y+1] == turn){
+            degree45 += 1;
+            if(x >= 2 && y+2 < 10 && board[x-2][y+2] == turn){
+            	degree45 += 1;
+            	if(x >= 3 && y+3 < 10 && board[x-3][y+3] == turn){
+                	degree45 += 1;
+                	if(x >= 4 && y+4 < 10 && board[x-4][y+4] == turn){
+                    	degree45 += 1;
+                    }
+                }
+            }
+        }
+        if(degree45 >= 4){
+            return turn;
+        }
+        
+        if(y >= 1 && board[x][y-1] == turn){
+            degree0 += 1;
+            if(y >= 2 && board[x][y-2] == turn){
+            	degree0 += 1;
+            	if(y >= 3 && board[x][y-3] == turn){
+                	degree0 += 1;
+                	if(y >= 4 && board[x][y-4] == turn){
+                    	degree0 += 1;
+                    }
+                }
+            }
+        }
+        if(degree0 >= 4){
+            return turn;
+        }
+        
+        return 0;
     }
     
-    function getStatus() public returns(uint8){
+    function getStatus() view public returns(uint8){
         return turn;
     }
     
